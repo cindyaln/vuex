7400a0583f930c12147e16aa2ecc76d22080558c
unknown <cindyauliani0629@gmail.com>
2023-08-08 23:13:44 -0300

    vuex

---
+++ src/store/modules/donatur.js
@@ -0,0 +1,18 @@
+const donatur = {
+    namespaced: true,
+    state: {
+        listDonatur: [
+            { name: "cindy" },
+            { name: "auliani" },
+            
+        ],
+    },
+    mutations: {
+        // NONE
+    },
+    actions: {
+        // NONE 
+    },
+};
+
+export default donatur;  
